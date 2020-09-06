37298f9f2e64795bfb66934e113a0332986f98d3
angels101 <angelscodex101@gmail.com>
2020-09-06 11:51:08 +0300

    the inquote to add event emmiter to be able to display the quote from the forrm

---
+++ src/app/inquote/inquote.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InquoteComponent } from './inquote.component';
+
+describe('InquoteComponent', () => {
+  let component: InquoteComponent;
+  let fixture: ComponentFixture<InquoteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InquoteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InquoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/inquote/inquote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inquote',
+  templateUrl: './inquote.component.html',
+  styleUrls: ['./inquote.component.css']
+})
+export class InquoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
